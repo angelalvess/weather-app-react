38390ca2fe21ad44d2f0d52edc00e70283bc7b7b
angelalvess <angeltrashangel@gmail.com>
2024-03-18 08:35:38 -0300

    finish fixing

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,18 +6,17 @@ function Card({ data }) {
   const { location, current } = data;
 
   return (
-    <div className="bg-white p-6 mt-8 rounded-lg shadow-md mr-20 min-w-[220px]">
+    <div className="bg-white p-6 mt-8 rounded-lg shadow-md mr-20 min-w-[300px] h-[350px] ">
       <div className="text-center">
         <span className="block text-xl font-bold text-slate-700 ">
           {location.name}
-          {', '}
         </span>
         <span className="text-slate-500 text-sm font-medium">
           {`${location.region}, ${location.country}`}
         </span>
       </div>
 
-      <div className="flex justify-center items-center ml-3 mt-14 mb-7">
+      <div className="flex justify-center items-center ml-5 mt-14 mb-7">
         <span className="text-7xl font-bold text-slate-700">
           {current.temp_c}
         </span>
@@ -26,7 +25,7 @@ function Card({ data }) {
 
       <div className="flex justify-center flex-col items-center ">
         <img src={current.condition.icon} alt="Weather" />
-        <span className="text-slate-500 font-medium">
+        <span className="text-slate-700 font-medium">
           {current.condition.text}
         </span>
       </div>
